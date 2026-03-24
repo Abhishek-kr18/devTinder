1a93da5527a4031c44a58b8b9d144d7f31409afb
Abhishek Kumar <abhishekkrr2203@gmail.com>
2026-03-24 18:28:15 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,22 @@ const express =require("express");
 
 const app=express();
 
-app.use((req,res)=>{
-    res.send("hello from the server!");
-})
+app.use("/hello",(req,res)=>{
+    res.send("hello hello hello hello");
+});
+
+app.get("/user",(req,res)=>{
+    res.send({firstName : "Abhishek",lastName : "kumar",email: "abc@gmail.com"});
+});
+
+app.post("/user",(req,res)=>{
+    res.send("successfully saved the data!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("successfully deleted the data!");
+});
+
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000...");
