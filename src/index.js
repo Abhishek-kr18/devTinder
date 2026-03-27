a705d359a680a2faf52f10b7edf3055078960c1a
Abhishek Kumar <abhishekkrr2203@gmail.com>
2026-03-27 14:58:19 +0530

    schema changes

--- src/index.js
+++ src/index.js
@@ -6,20 +6,60 @@ import User from "./models/user.js";
 
 const app=express();
 
+app.use(express.json());
+
 app.post("/signup", async (req,res)=>{
+    console.log(req.body);
     //creating a new instance of a user model
-    const user=new User({
-         firstName:"ab",
-         lastName:"Ku",
-         emailId:"abc@gmail.com",
-         phoneNo:"7859028434"
-    });
+    const user=new User(req.body);
    try{
     await  user.save();
    res.send("user added successfully!");
 }catch (err){
     res.status(400).send("Error saving the user:" +err.message);
 }
+ });
+
+ //get user by email
+ app.get("/user",async (req,res)=>{
+    const userEmail=req.body.emailId;
+
+
+    try{
+        const user=await  User.find({emailId:userEmail});
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+
+    }
+ });
+
+ app.get("/feed",(req,res)=>{
+
+ });
+
+ app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    try{ 
+        const user=await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully!");
+
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+
+ });
+
+ app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("user updated successfully!");
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
 });
 
 connectDB() .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,39 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
   firstName: {
-    type: String
+    type: String,
+    required :true
   },
   lastName: {
     type: String
   },
   emailId: {
-    type: String
+    type: String,
+    required :true,
+    unique :true
   },
   password: {
-    type: Number  
+    type: String 
   },
   age: {
     type: Number
   },
   gender: {
     type: String
+  },
+  photoUrl: {
+    type: String,
+    default:"https://imgs.search.brave.com/ym-_u0PNwGk8QnySE3Hq9Iw9wPNY7xzwOBiXXiqzvwE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMTQ4/NTYxMDUvcGV4ZWxz/LXBob3RvLTE0ODU2/MTA1LmpwZWc_YXV0/bz1jb21wcmVzcyZj/cz10aW55c3JnYiZk/cHI9MSZ3PTUwMA"
+  },
+  about: {
+    type: String,
+    default: "Hey there! I'm using DevTinder."
+  },
+  skills: {
+    type: [String]
   }
+}, {
+  timestamps: true
 });
 
 export default model("User", userSchema);
